b9b1c1a987ad98052a747e3e698a894b6a56ddb5
GeorgHerych <168106710+GeorgHerych@users.noreply.github.com>
2025-08-23 11:03:08 +0300

    Add Shtat and KTVP buttons to Form1

--- Personal+/Form1.Designer.cs
+++ Personal+/Form1.Designer.cs
@@ -54,6 +54,8 @@
             this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
             this.label10 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
@@ -262,16 +264,33 @@
             this.label10.Size = new System.Drawing.Size(104, 23);
             this.label10.TabIndex = 22;
             this.label10.Text = "Version beta";
-            // 
+            //
             // button1
-            // 
+            //
             this.button1.Location = new System.Drawing.Point(1243, 281);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 33);
             this.button1.TabIndex = 23;
             this.button1.Text = "Особовий склад";
             this.button1.UseVisualStyleBackColor = true;
-            // 
+            //
+            // button2
+            //
+            this.button2.Location = new System.Drawing.Point(1243, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 33);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Штат";
+            this.button2.UseVisualStyleBackColor = true;
+            //
+            // button3
+            //
+            this.button3.Location = new System.Drawing.Point(1243, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 33);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "КТВП";
+            this.button3.UseVisualStyleBackColor = true;
             //
             // button4
             //
@@ -421,6 +440,8 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -486,6 +507,8 @@
         private System.DirectoryServices.DirectorySearcher directorySearcher1;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
